94becf3213114c814761c3d9e9e68b7b327d5d5e
Tomas Sykora, jr <jump@syky.cz>
2017-10-08 00:52:49 +0200

    Carthage support

---
+++ TRTabViewFramework/TRTabViewFramework.h
@@ -0,0 +1,23 @@
+//
+//  TRTabViewFramework.h
+//  TRTabViewFramework
+//
+//  Created by Tomas Sykora, jr. on 08/10/2017.
+//  Copyright © 2017 Matthias Keiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRTabViewFramework.
+FOUNDATION_EXPORT double TRTabViewFrameworkVersionNumber;
+
+//! Project version string for TRTabViewFramework.
+FOUNDATION_EXPORT const unsigned char TRTabViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRTabViewFramework/PublicHeader.h>
+
+
+#import "TRViewController.h"
+#import "TRContentViewController.h"
+#import "TRTabView.h"
+#import "TRTab.h"
